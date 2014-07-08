5bbbe7c2ac5b9678037d8287f9a294b38751f44f
Jochen Wersdörfer <jochen@wersdoerfer.de>
2014-07-08 17:48:05 +0200

    added local memory cache + removed secret key file (set in env)

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -23,12 +23,6 @@ def _update_settings(source_folder, site_name):
         'ALLOWED_HOSTS =.+$',
         'ALLOWED_HOSTS = ["%s"]' % (site_name,)
     )
-    secret_key_file = source_folder + '/ml_jobcontrol/secret_key.py'
-    if not exists(secret_key_file):
-        chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
-        key = ''.join(random.SystemRandom().choice(chars) for _ in range(50))
-        append(secret_key_file, "SECRET_KEY = '%s'" % (key,))
-    append(settings_path, '\nfrom .secret_key import SECRET_KEY')
 
 def _update_virtualenv(source_folder):
     virtualenv_folder = source_folder + '/../virtualenv'
